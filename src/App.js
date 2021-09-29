3b02acf0712d6cd37cebb96155302e44ab57904f
mayanja andrew <nakanwagitreasure@gmail.com>
2021-09-29 16:37:16 +0300

    final changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
+    <div>
+       <p>
         Lets proceed forward!
         </p>
-          Learn React Fellas
-      </header>
     </div>
   );
 }
