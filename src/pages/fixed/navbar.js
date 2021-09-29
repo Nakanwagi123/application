7c9b259b85e2898b7e09f4d3f21fff07cbae559a
mayanja andrew <nakanwagitreasure@gmail.com>
2021-09-29 17:48:33 +0300

    genesis

--- src/pages/fixed/navbar.js
+++ src/pages/fixed/navbar.js
@@ -8,14 +8,23 @@ function Navbar(){
         <div class="container">
         <div class="split">
         <div class="button"><i class="fas fa-bars"></i></div>
+        <div class="overall">
         <div class="profile">
         Profile
         </div>
-        <div class="dropdown"><ul><li>RideHistory</li><li>Support</li><li>About</li></ul></div>
+        <div class="dropdown">
+        <ul>
+        <li>RideHistory</li>
+        <li>Support</li>
+        <li>About</li>
+        </ul>
+        </div>
+        </div>
+        <p>hello</p>
         <div class="rest">Signup to drive</div>
         </div>
         </div>
-        </section>
+    </section>
         </>
         )
 }
