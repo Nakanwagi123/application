48f8ada43dfb1ac5e504f872966d91d3c69e6c55
mayanja andrew <nakanwagitreasure@gmail.com>
2021-09-29 21:47:49 +0300

    code

--- src/pages/fixed/Nav.js
+++ src/pages/fixed/Nav.js
@@ -2,28 +2,30 @@ import React from "react";
 import "./navbar.css";
 
 function Nav(){
+
+    
     return(
     <>
     <section>
         <div class="container">
         <div class="split">
-        <div class="button"><i class="fas fa-bars"></i></div>
+        <div class="button"><button class="menu"><i class="fas fa-bars"></i></button></div>
+
         <div class="overall">
         <div class="profile">
         Profile
         </div>
         <div class="dropdown">
-        <ul>
+        <ul class="options">
         <li>Home</li>
         <li>RideHistory</li>
         <li>Support</li>
         <li>About</li>
         </ul>
         </div>
-        </div>
-        <p>hello</p>
         <div class="rest">Signup to drive</div>
         </div>
+        </div> 
         </div>
     </section>
         </>
