b3dec0f3ca4111353b44bd56e655445c3cba18f7
mayanja andrew <nakanwagitreasure@gmail.com>
2021-09-30 15:47:13 +0300

    changes

--- src/pages/fixed/Nav.js
+++ src/pages/fixed/Nav.js
@@ -10,14 +10,14 @@ function Nav(){
         <h1 class="logo">Logo</h1>
         <input type="checkbox" id="nav-toggle" class="nav-toggle"/>
         <nav>
-        <div class="profile"><h2>Profile</h2></div>
             <ul>
+            <li><a href="#">Profile</a></li>
                 <li><a href="#">Menu</a></li>
                 <li><a href="#">RideHistory</a></li>
                 <li><a href="#">Support</a></li>
                 <li><a href="#">About</a></li>
+                <li><a href="#">Signup to drive</a></li>
             </ul>
-            <div class="rest">Signup to drive</div>
         </nav>
         <label for="nav-toggle" class="nav-toggle-label"><span></span></label>
     </header>
