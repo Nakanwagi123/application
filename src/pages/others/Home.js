a6cd2435a9cef310e3d68a54bb42dd006dac81e4
mayanja andrew <nakanwagitreasure@gmail.com>
2021-09-30 20:55:15 +0300

    changes

--- src/pages/others/Home.js
+++ src/pages/others/Home.js
@@ -4,16 +4,26 @@ import './Home.css';
 function Home(){
     return(
     <>
-    <div class="contain"><div><p>This company is all about winning go on you guys</p>Text goes here</div>
-        <div><p>This company is all about winning go on you guys</p>Text goes here</div>
-        <div><p>This company is all about winning go on you guys</p>Text goes here</div>
-        <div><p>This company is all about winning go on you guys</p>Text goes here</div>
-        <div><p>This company is all about winning go on you guys</p>Text goes here</div>
-        <div><p>This company is all about winning go on you guys</p>Text goes here</div>
-        <div><p>This company is all about winning go on you guys</p>Text goes here</div>
-        <div><p>This company is all about winning go on you guys</p>Text goes here</div>
-        <div><p>This company is all about winning go on you guys</p>Text goes here</div>
-    </div>
+   <section class="order">
+       <div class="container">
+           <div class="split">
+           <div class="placeorder"><button><h4>Order a Ride</h4></button></div>
+           <div class="slider">picture goes here</div>
+           <div class="welcome"><h2>WELCOME TO RabEat</h2></div>
+           </div>
+       </div>
+   </section>
+   <section>
+       <div class="offers">
+           <h3>Top Offers</h3>
+           <ul>
+               <li><h4>Kampala</h4><p>Travel to Kampala</p></li>
+               <li><h4>Wakiso</h4><p>Travel to Wakiso</p></li>
+               <li><h4>Ntinda</h4><p>Travel to Ntinda</p></li>
+               <li><h4>Kibuye</h4><p>Travel to Kibuye</p></li>
+           </ul>
+           </div> 
+   </section>
         
     </>);
 }
