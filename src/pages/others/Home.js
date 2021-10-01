256d4e32934571ce5db43a272900db8ea696bc70
mayanja andrew <nakanwagitreasure@gmail.com>
2021-10-01 11:14:34 +0300

    changes

--- src/pages/others/Home.js
+++ src/pages/others/Home.js
@@ -7,23 +7,22 @@ function Home(){
    <section class="order">
        <div class="container">
            <div class="split">
-           <div class="placeorder"><button><h4>Order a Ride</h4></button></div>
-           <div class="slider">picture goes here</div>
-           <div class="welcome"><h2>WELCOME TO RabEat</h2></div>
+           <div class="placeorder"><p>Lets get you there!</p><button><h4>Order a Ride</h4></button></div>
+           <div class="slider"><p>hello</p></div>
+           <div class="welcome"><p>WELCOME TO RIDEMYWAY!</p></div>
            </div>
        </div>
    </section>
    <section>
-       <div class="offers">
-           <h3>Top Offers</h3>
-           <ul>
-               <li><h4>Kampala</h4><p>Travel to Kampala</p></li>
-               <li><h4>Wakiso</h4><p>Travel to Wakiso</p></li>
-               <li><h4>Ntinda</h4><p>Travel to Ntinda</p></li>
-               <li><h4>Kibuye</h4><p>Travel to Kibuye</p></li>
-           </ul>
-           </div> 
+       <div class="containerone">
+           <div class="split">
+               <div class="title">
+                   <p>BROWSE CATEGORIES</p><p><hr/></p>
+                   </div>
+           </div>
+       </div>
    </section>
+  
         
     </>);
 }
